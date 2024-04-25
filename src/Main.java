400a764c2e5916a6d19c651754482ceb63b57d1c
ohyeongseok <dhgudtjr6635@gmail.com>
2024-04-25 11:44:52 +0900

    나이 맞추기 프로그램

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static int solution(int age) {
+        int answer = 2023-age;
+        return answer;
+    }
+    public static void main(String[] args) {
+        int age = 24;
+        System.out.println(solution(age));
+    }
+}
